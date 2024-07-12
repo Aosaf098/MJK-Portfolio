48c98b3c4aa11a7780dee9f4b2f26378b99780b0
Aosaf098 <aosafibad10@gmail.com>
2024-07-12 19:56:07 +0600

    Quote Slider Ongoing

---
+++ slider.js
@@ -0,0 +1,50 @@
+var current = 1;
+var max = $("li").length + 1;
+
+$(".quotation").hide();
+$(".quotation:nth-child(1)").fadeIn("slow");
+
+//function to change to next quote
+
+function changeUp(){
+    $(".quotation").hide();
+    current += 1;
+    if (current === max) {
+        current = 1;
+    }
+    $(".quotation:nth-child(" + current + ")").fadeIn("slow");
+}
+
+function changeDown() {
+    $(".quotation").hide();
+    current -= 1;
+
+    if(current === 0){
+        current = max -1;
+    }
+
+    $(".quotation:nth-child(" + current +")").fadeIn("slow");
+}
+
+startChange();
+
+$(".slider-arrow").click(function(){
+    stopChange();
+    changeUp();
+    startChange();
+});
+
+$(".slider-arrow .prev").click(function(){
+    stopChange();
+    changeDown();
+    startChange();
+});
+
+function startChange(){
+    changeIt = setInterval(function(){
+        changeUp();
+    }, 3000)
+    }
+function stopChange() {
+    clearInterval(changeIt);
+}
